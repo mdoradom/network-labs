c7d184323de4801ca53cab761468a28de0413278
Mario Dorado Martínez <44005739+mdoradom@users.noreply.github.com>
2025-10-14 08:46:26 +0200

    base files

---
+++ lab3/readFile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class readFile : MonoBehaviour
+{
+ void deserialize()
+    {
+        MemoryStream ms = new MemoryStream();
+        using (FileStream fs = File.OpenRead(Application.dataPath + ""))
+        {
+            fs.CopyTo(ms);
+        }
+    }
+}
